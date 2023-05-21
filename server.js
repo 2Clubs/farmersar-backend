2d20cc15b52ab6d17e0e401cd803ec83426ff573
2clubs <bennettdan85@gmail.com>
2023-05-21 18:33:49 -0400

    adds middleware

--- server.js
+++ server.js
@@ -30,8 +30,12 @@ mongoose.connection
     .on('error', (error) => console.log(error))
 
 /////////////////////////////
-// MODELS
+// MIDDLEWARE
 /////////////////////////////
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 /////////////////////////////
 // ROUTES
