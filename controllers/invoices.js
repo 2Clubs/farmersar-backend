2b8bbee9407ff23fb6c9b37245ffa2afdab16bea
2clubs <bennettdan85@gmail.com>
2023-05-21 18:12:54 -0400

    adds invoices routes

---
+++ controllers/invoices.js
@@ -0,0 +1,27 @@
+/////////////////////////////
+// DEPENDENCIES
+/////////////////////////////
+const express = require('express')
+const invoicesRouter = express.Router()
+const Invoice = require('../models/invoices')
+
+/////////////////////////////
+// ROUTES
+/////////////////////////////
+// Index
+invoicesRouter.get('/', async (req, res) => {
+    try {
+        res.json(await Invoice.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+// Create
+invoicesRouter.post('/', async (req, res) => {
+    try {
+        res.json(await Invoice.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+module.exports = invoicesRouter
