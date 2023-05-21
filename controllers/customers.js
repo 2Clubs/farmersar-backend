c075b869491951a63679eb6f7e7b3298173ebbdf
2clubs <bennettdan85@gmail.com>
2023-05-21 16:07:49 -0400

    adds controllers

--- controllers/customers.js
+++ controllers/customers.js
@@ -4,3 +4,24 @@
 const express = require('express')
 const customersRouter = express.Router()
 const Customer = require('../models/customers')
+
+/////////////////////////////
+// ROUTES
+/////////////////////////////
+// Index
+customersRouter('/', async (req, res) => {
+    try {
+        res.json(await Customer.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+// Create
+customersRouter('/', async (req, res) => {
+    try {
+        res.json(await Customer.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+module.exports = customersRouter
